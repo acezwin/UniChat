350edc29cd252c884417a6944805b928436795a0
acezwin <mwin2588@gmail.com>
2022-04-21 19:59:34 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require ('express')
+const expressLayouts = require('express-ejs-layouts')
+const app = express()
+const port = 8080
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+//app.use('/js', express.static(__dirname + 'public/js'))
+//app.use('/img', express.static(__dirname + 'public/img'))
+
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+//navs
+app.get('', (reg, res) => {
+    res.render('index', {title: 'UniChat: Welcome!'})
+})
+app.get('/about', (reg, res) => {
+    res.render('about', {title: 'About Page'})
+})
+app.get('/explore', (reg, res) => {
+    res.render('explore', {title: 'Colleges'})
+})
+app.get('/chatBot', (reg, res) => {
+    res.render('chatbot', {title: 'UniChat'})
+})
+
+app.listen(port, () => 
+    console.info('App is listening on port 8080')
+)
