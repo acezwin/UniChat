ee019d83fd7c0c1f2b47c2258de0980c54df1b0b
k3ls33 <kelseyllohr1@gmail.com>
2022-05-03 09:35:05 -0400

    ajax post fix

--- settings.js
+++ settings.js
@@ -1,20 +1,16 @@
 function applySettings() {
     var filterValues = [];
 
-    $("apply").click(function(e) {
-        e.preventDefault();
+    $("input[name='filters']:checked").each(function () {
+        filterValues.push($(this).val());
+    });
 
-        $("input[name='filters']:checked").each(function() {
-            filterValues.push($(this).val());
-        });
-        
-        $.ajax({
-            type: "POST",
-            url: "/filters",
-            data: JSON.stringify(filterValues),
-            contentType: "application/json",
-            dataType: 'html' 
-        });
+    $.ajax({
+        type: "POST",
+        url: "/filters",
+        data: JSON.stringify(filterValues),
+        contentType: "application/json",
+        dataType: 'html'
     });
 }
 
